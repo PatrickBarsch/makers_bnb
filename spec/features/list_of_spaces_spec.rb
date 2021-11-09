ef1f5337c040d4d267f52a3974bea0d14b5a05d4
Rebecca <Beckyrose200@aol.com>
2021-11-09 11:19:31 +0000

    Added set_up_test_database methods for feature tests

--- spec/features/list_of_spaces_spec.rb
+++ spec/features/list_of_spaces_spec.rb
@@ -1,7 +1,7 @@
 feature 'Spaces' do
   scenario 'See the list of spaces' do
-    connection = PG.connect(dbname: 'makers_bnb_test')
-    connection.exec("INSERT INTO spaces (name) VALUES ('Space 1);")
+    setup_test_database
+    add_row_to_test_database
     visit('/spaces')
     expect(page).to have_content 'Space 1'
   end 
