c846387b0bf222b2a30c5dbd70d490732b65fe05
Rebecca <Beckyrose200@aol.com>
2021-11-09 12:36:20 +0000

    Added borders and cosmetics for spaces page

--- app.rb
+++ app.rb
@@ -4,8 +4,10 @@ require 'pg'
 class MakersBnb < Sinatra::Base
 
   get '/spaces' do
-    connection = PG.connect(dbname: 'makers_bnb_test') 
+    connection = PG.connect(dbname: 'makers_bnb_test')
+    # connection.exec("INSERT INTO spaces (name) VALUES ('Space 1');") 
     @result = connection.exec("SELECT * FROM spaces")
+    @display = @result.map { |spaces| spaces['name'] }
     erb :spaces
   end 
 
