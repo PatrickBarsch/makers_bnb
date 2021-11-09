edb1eebcd2aa1715b7027057d982de07747b4ff1
Rebecca <Beckyrose200@aol.com>
2021-11-09 11:47:59 +0000

    Spaces views displays the makers_bnb database

--- app.rb
+++ app.rb
@@ -1,7 +1,13 @@
 require 'sinatra/base'
+require 'pg'
 
 class MakersBnb < Sinatra::Base
+
   get '/spaces' do
+    connection = PG.connect(dbname: 'makers_bnb_test') 
+    @result = connection.exec("SELECT * FROM spaces")
     erb :spaces
   end 
+
+  run! if app_file == $0
 end
