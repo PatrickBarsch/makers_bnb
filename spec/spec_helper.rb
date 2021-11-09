2418301b4e88d8adbd8ae80f8eb2716f3680c780
PatrickBarsch <PatrickGB@gmx.de>
2021-11-09 12:54:19 +0100

    Activated SimpleCov Formatter

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,12 @@ require 'simplecov'
 require 'simplecov-console'
 require_relative './setup_test_database'
 
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
+  SimpleCov::Formatter::Console,
+  SimpleCov::Formatter::HTMLFormatter
+])
+SimpleCov.start
+
 require File.join(File.dirname(__FILE__), '..', 'app.rb')
 Capybara.app = MakersBnb
 ENV['ENVIRONMENT'] = 'test'
