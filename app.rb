d598672058cb024b633f1dcae3ce11afd9d0dd88
RumenKanev <rkynev@gmail.com>
2021-11-10 12:10:40 +0000

    pages with test

--- app.rb
+++ app.rb
@@ -2,18 +2,38 @@ require 'sinatra/base'
 require 'pg'
 
 class MakersBnb < Sinatra::Base
-
-  get '/spaces' do
-    connection = PG.connect(dbname: 'makers_bnb_test')
-    connection.exec("INSERT INTO spaces (name) VALUES ('Space 1');") 
-    @result = connection.exec("SELECT * FROM spaces")
-    @display = @result.map { |spaces| spaces['name'] }
-    erb :spaces
-  end 
-  
+    # ----- Book a Space -----
+    get '/spaces' do
+      connection = PG.connect(dbname: 'makers_bnb_test')
+      connection.exec("INSERT INTO spaces (name) VALUES ('Space 1');") 
+      @result = connection.exec("SELECT * FROM spaces")
+      @display = @result.map { |spaces| spaces['name'] }
+      erb :spaces
+    end 
+    # ----- List Space -----
     get '/spaces/new' do 
-    erb :list_space
-  end
+        erb :list_space
+    end
+    post '/list_space' do
+      redirect '/spaces'
+    end
+    # ----- Sign Up -----
+    get '/' do 
+        erb :sign_up
+    end
+    post '/sign_up' do
+      redirect '/sessions/new'
+    end
+
+    # ----- Log in -----
+    get '/sessions/new' do 
+        erb :login
+    end
+    post '/login' do
+       redirect '/spaces'
+    end
+    
+
 
   run! if app_file == $0
 
---
+++ spec/features/list_space_spec.rb
@@ -0,0 +1,16 @@
+feature 'List a space' do
+    scenario 'A user can list space using BnB' do
+      visit('/spaces/new')
+      fill_in('name', with: 'La Cotagge')
+      fill_in('description', with: 'Some description')
+      fill_in('price', with: 150)
+      fill_in('from_date', with: 10/11/2021)
+      fill_in('to_date', with: 20/11/2021)
+      click_button('List my Space')
+  
+    #   ---- To Be Changed -----
+      expect(page).to have_content('Sign out')
+      expect(page).to have_content('Book a Space')
+      expect(page).to have_content('List a space')
+    end
+  end
