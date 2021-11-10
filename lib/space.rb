685de25de33934b753712c953ca2893425dc6e76
PatrickBarsch <PatrickGB@gmx.de>
2021-11-10 08:24:57 +0100

    Refactored Space.list using DatabaseConnection properly

--- lib/space.rb
+++ lib/space.rb
@@ -2,7 +2,8 @@ require_relative './database_connection'
 
 class Space
   def self.list(name)
-    connection = DatabaseConnection.setup
-    connection.query("INSERT INTO spaces(name) VALUES($1) RETURNING id, name;", [name])
+    DatabaseConnection.setup
+    sql = "INSERT INTO spaces(name) VALUES($1) RETURNING id, name;"
+    DatabaseConnection.query(sql, [name])
   end
 end
