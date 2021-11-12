2b3acd76045762dbb322bc29ae23f8d8b3a1fb7e
PatrickBarsch <PatrickGB@gmx.de>
2021-11-12 12:06:40 +0100

    Refactored space tests with before block

--- spec/space_spec.rb
+++ spec/space_spec.rb
@@ -1,11 +1,21 @@
 describe Space do
+  before(:each) do
+    @space1 = { name: 'MyFancySpace', 
+      description: 'A small space', 
+      price_per_night: 80,
+      owner_id: 1 }
+    @space2 = { name: 'MyEvenFancierSpace', 
+      description: 'It is a lot bigger',
+      price_per_night: 150,
+      owner_id: 2  }
+    @space3 = { name: 'MyUltraFancySpace', 
+      description: 'I call it minimalistic',
+      price_per_night: 300,
+      owner_id: 3 }
+  end
   describe '.list' do 
     it 'adds a space to the list of spaces' do
-      space1 = { name: 'MyFancySpace', 
-        description: 'A small space', 
-        price_per_night: 80,
-        owner_id: 1  }
-      added = Space.list(**space1)
+      added = Space.list(**@space1)
       id_added = added.first['id']
       results = persisted_data(id: id_added, table: 'spaces')
       expect(added.first).to eq results
@@ -13,22 +23,9 @@ describe Space do
   end
   describe '.all' do
     it 'returns a list of all available spaces' do
-      space1 = { name: 'MyFancySpace', 
-        description: 'A small space', 
-        price_per_night: 80,
-        owner_id: 1 }
-      space2 = { name: 'MyEvenFancierSpace', 
-        description: 'It is a lot bigger',
-        price_per_night: 150,
-        owner_id: 2  }
-      space3 = { name: 'MyUltraFancySpace', 
-        description: 'I call it minimalistic',
-        price_per_night: 300,
-        owner_id: 3 }
-      
-      added = [Space.list(**space1)]
-      added << Space.list(**space2)
-      added << Space.list(**space3)
+      added = [Space.list(**@space1)]
+      added << Space.list(**@space2)
+      added << Space.list(**@space3)
       added.map! { |result| result.first }
       space_list = Space.all
       expect(space_list.length).to eq 3
